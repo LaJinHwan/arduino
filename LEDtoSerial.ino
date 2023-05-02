54617e8c9079c0b111de1a411f8ad34bbf17c37f
LaJinHwan <73628829+LaJinHwan@users.noreply.github.com>
2023-05-02 10:12:07 +0900

    Update LEDtoSerial.ino

--- LEDtoSerial.ino
+++ LEDtoSerial.ino
@@ -1,5 +1,6 @@
 const int switchPin = 2;
 const int ledPin = 13;
+const int relay = 9;
 bool isButtonPressed = false;
 unsigned long previousMillis = 0;
 unsigned long interval = 0;
@@ -11,6 +12,7 @@ void setup()
 {
   pinMode(switchPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
+  pinMode(relay,OUTPUT);
   Serial.begin(115200);
 }
 void swi()
@@ -40,37 +42,7 @@ void swi()
       if (isLongPressed)
       {
         digitalWrite(ledPin, LOW);
-        while (1)
-        {
-
-          if (digitalRead(switchPin) == LOW)
-          {
-            isButtonPressed = false;
-            isLongPressed = false;
-            break;
-          }
-          else if (Serial.available() > 0)
-          {
-            String input = Serial.readStringUntil('\n');
-            input.trim();
-
-            if (input == "one")
-            {
-              interval = 1000;
-              break;
-            }
-            else if (input == "two")
-            {
-              interval = 100;
-              break;
-            }
-            else if (input == "three")
-            {
-              interval = 0;
-              break;
-            }
-          }
-        }
+        pinlow();
       }
       else
       {
@@ -107,68 +79,106 @@ void blink()
   {
     previousMillis = millis();
     digitalWrite(ledPin, !digitalRead(ledPin));
+    digitalWrite(relay,!digitalRead(relay));
   }
 }
 
 void seri()
+
 {
-  if (Serial.available() > 0)
-  {
+if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
     input.trim();
-
-    if (input == "one")
-    {
+    if (input == "one") {
       interval = 1000;
-    }
-    else if (input == "two")
-    {
+    } else if (input == "two") {
       interval = 100;
-    }
-    else if (input == "three")
-    {
+    } else if (input == "three") {
       interval = 0;
-    }
-    else if (input == "zero")
-    {
+    } else if (input == "zero") {
       digitalWrite(ledPin, LOW);
-      while (1)
-      {
-
-        if (digitalRead(switchPin) == LOW)
-        {
-          isButtonPressed = false;
-          isLongPressed = false;
-          break;
-        }
-        else if (Serial.available() > 0)
-        {
-          String input = Serial.readStringUntil('\n');
-          input.trim();
-
-          if (input == "one")
-          {
-            interval = 1000;
-            break;
-          }
-          else if (input == "two")
-          {
-            interval = 100;
-            break;
-          }
-          else if (input == "three")
-          {
-            interval = 0;
-            break;
-          }
-        }
-      }
+      pinlow();
+    } else if (input == "sos") {
+      
+      sos();
     }
   }
 }
-void loop()
-{
+
+void sos() {
+  interval = 1000;
+  unsigned long currentMillis = millis();
+
+  for (int j = 1; j < 4; j++) {
+    for (int i = 0; i < 6; i++) {
+      if (i % 2 == 0) {
+        digitalWrite(ledPin, HIGH);digitalWrite(relay, HIGH);
+      } else {
+        digitalWrite(ledPin, LOW);digitalWrite(relay, LOW);
+      }
+      
+      if (j == 1 || j == 3) {
+        if (i == 0 || i == 4) {
+          
+        } else if (i == 2) {
+          
+        }
+      } else if (j == 2) {
+        if (i == 0 || i == 4) {
+          digitalWrite(relay, LOW);
+        } else if (i == 2) {
+          digitalWrite(relay, HIGH);
+        }
+      }
+      
+      if (j % 2 == 0) {
+        interval = 200;
+      } else {
+        interval = 500;
+      }
+      
+      while (millis() - currentMillis < interval / 2) { }
+      currentMillis = millis();
+     
+    }
+  }
+
+ digitalWrite(ledPin,LOW);
+ pinlow();
+}
+
+void pinlow(){
+    while (1) {
+        if (digitalRead(switchPin) == LOW) {
+          isButtonPressed = false;
+          isLongPressed = false;
+          break;
+        } else if (Serial.available() > 0) {
+          String input = Serial.readStringUntil('\n');
+          input.trim();
+          if (input == "one") {
+            interval = 1000;
+            break;
+          } else if (input == "two") {
+            interval = 100;
+            break;
+          } else if (input == "three") {
+            interval = 0;
+            break;
+          } else if (input == "sos"){
+            sos();
+          }
+        }
+      }
+}
+
+
+\
+
+void loop() {
   swi();
   blink();
   seri();
+  //relayto();
+  
 }
